cec685da6014bef5e59d5b27a673a2ffd57f612c
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2025-05-30 17:18:56 -0300

    ADD:Objetos

--- a_main.py
+++ a_main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from personagem import Morgana
+from obstaculos import Obstaculo
 pg.init() #Configurações iniciais
 r = pg.time.Clock()
 
@@ -9,9 +10,14 @@ pg.display.set_caption("Lady Morgana") #Nome do jogo
 
 morgana = Morgana("imagem/morgana_.png",140,235,550,550)
 
-fundo = pg.image.load ("imagem/Cenário_02.png") #Decidindo o cenário
+fundo = pg.image.load ("imagem/cenario_02.png") #Decidindo o cenário
 tela.blit (fundo,(0,0)) #Desenhando o cenário
-
+o = [Obstaculo("imagem/pocao_a.png",70,70),
+     Obstaculo("imagem/pocao_m.png",70,70),
+     Obstaculo("imagem/vinho.png",70,70),
+     Obstaculo("imagem/agua.png",70,70),
+     Obstaculo("imagem/morcego.png",80,80)]
+     
 
 #Se não colocar em loop a tela do jogo não permanece.
 jogo_ligado = True
@@ -20,8 +26,11 @@ while jogo_ligado:
         if evento.type == pg.QUIT:
             jogo_ligado = False
     
+
     tela.blit (fundo,(0,0))
     tela.blit (morgana.imagem,(morgana.p_x,morgana.p_y))
+    for ob in o:
+        tela.blit (ob.imagem,(ob.p_x,ob.p_y))
 
     morgana.moviment(pg.K_RIGHT,pg.K_LEFT)
     #Permanencia de atualizações
--- obstaculos.py
+++ obstaculos.py
@@ -1,1 +1,18 @@
 import pygame as pg
+import random
+
+class Obstaculo:
+    def __init__(self,end_imagem, la, al):
+
+        self.la = la
+        self.al = al
+        self.imagem = pg.image.load (end_imagem)
+        self.imagem = pg.transform.scale(self.imagem,(self.la,self.al))
+
+        self.p_yi  = 0
+        self.lista_pos = [200,300,400,500,600,700,800,900,1000]
+
+        self.p_y =  self.p_yi
+        self.p_x = random.choice(self.lista_pos)
+
+        self.velo = (random.randint(3,6))
