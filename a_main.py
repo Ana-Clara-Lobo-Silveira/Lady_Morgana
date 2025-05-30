620532ec795b9247a156d1f795ed40833b008fc8
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2025-05-30 16:13:04 -0300

    ADD:Movimento

--- a_main.py
+++ a_main.py
@@ -1,9 +1,17 @@
 import pygame as pg
+from personagem import Morgana
 pg.init() #Configurações iniciais
 r = pg.time.Clock()
 
 tela = pg.display.set_mode((1200,800)) #Criando tela
 pg.display.set_caption("Lady Morgana") #Nome do jogo
+
+
+morgana = Morgana("imagem/morgana_.png",140,235,550,550)
+
+fundo = pg.image.load ("imagem/Cenário_02.png") #Decidindo o cenário
+tela.blit (fundo,(0,0)) #Desenhando o cenário
+
 
 #Se não colocar em loop a tela do jogo não permanece.
 jogo_ligado = True
@@ -11,8 +19,11 @@ while jogo_ligado:
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             jogo_ligado = False
+    
+    tela.blit (fundo,(0,0))
+    tela.blit (morgana.imagem,(morgana.p_x,morgana.p_y))
 
-
+    morgana.moviment(pg.K_RIGHT,pg.K_LEFT)
     #Permanencia de atualizações
     pg.display.update()
     r.tick(60)
---
+++ personagem.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+class Morgana:
+    def __init__(self,end_imagem, l, a, xi, yi): #Define o personagem com todos os parâmetros definidos de uma só vez.
+        self.imagem = pg.image.load (end_imagem)
+        self.imagem = pg.transform.scale(self.imagem,(l,a))
+
+        self.p_x = xi
+        self.p_y = yi
+
+        self.la = l
+        self.al = a
+
+    def moviment(self,t_d,t_e):
+        t = pg.key.get_pressed()
+        if t [t_d] and self.p_x <= 1200-self.la:
+            self.p_x += 5
+        elif t [t_e] and self.p_x >= 0:
+            self.p_x -= 5
